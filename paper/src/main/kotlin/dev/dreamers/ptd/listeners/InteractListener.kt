8596d81d48047276b35aeacef63bdfd76cda4cf9
Rafi <rafiei.default@gmail.com>
2025-02-11 18:36:37 +0330

    Finish handling interact event

--- paper/src/main/kotlin/dev/dreamers/ptd/listeners/InteractListener.kt
+++ paper/src/main/kotlin/dev/dreamers/ptd/listeners/InteractListener.kt
@@ -1,7 +1,12 @@
 package dev.dreamers.ptd.listeners
 
 import dev.dreamers.ptd.helpers.InventoryHelper
+import dev.dreamers.ptd.helpers.MessageHelper
+import dev.dreamers.ptd.services.MessageService
+import net.kyori.adventure.text.TextReplacementConfig
 import org.bukkit.Material
+import org.bukkit.block.Container
+import org.bukkit.block.EnderChest
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
@@ -10,40 +15,72 @@ import org.bukkit.event.player.PlayerInteractEvent
 class InteractListener : Listener {
     @EventHandler
     private fun onInteract(event: PlayerInteractEvent) {
-        if (event.action != Action.LEFT_CLICK_BLOCK) return
-        if (!event.player.hasPermission("ptd.events.interact")) return
+        val player = event.player
+        val block = event.clickedBlock ?: return
+
         if (
-            event.clickedBlock == null ||
-                (event.clickedBlock?.type != Material.CHEST &&
-                    event.clickedBlock?.type != Material.ENDER_CHEST)
+            event.action != Action.LEFT_CLICK_BLOCK ||
+                !player.hasPermission("ptd.events.interact") ||
+                player.inventory.itemInHand.type == Material.AIR ||
+                block.state !is EnderChest && block.state !is Container
         )
             return
-        if (event.player.itemInHand.type == Material.AIR) return
 
-        val player = event.player
-        val block = event.clickedBlock
-        val blockInventory = InventoryHelper.getInventory(player, block!!) ?: return
-        val item = player.itemInHand
+        val blockInventory = InventoryHelper.getInventory(player, block) ?: return
+        val item = player.inventory.itemInMainHand
+        val itemType = item.type
+        var totalTransfer = 0
 
         if (player.isSneaking) {
-            for (slot in 0 until player.inventory.size) {
-                val inventoryItem = player.inventory.getItem(slot) ?: continue
-                if (inventoryItem.type != item.type) continue
+            player.inventory.storageContents.forEachIndexed { index, slot ->
+                if (slot?.type == itemType) {
+                    val transferred =
+                        InventoryHelper.transferItems(blockInventory, itemType, slot.amount)
+                    totalTransfer += transferred
 
-                val remainingItems = blockInventory.addItem(inventoryItem)
-                player.inventory.setItem(slot, null)
+                    if (transferred == slot.amount) {
+                        player.inventory.setItem(
+                            index,
+                            null,
+                        )
+                    } else {
+                        slot.amount -= transferred
+                    }
 
-                if (remainingItems.isNotEmpty()) {
-                    player.inventory.setItem(slot, remainingItems.values.first())
-                    break
+                    if (transferred > 0) return@forEachIndexed
                 }
             }
         } else {
-            player.setItemInHand(null)
-            val remainingItems = blockInventory.addItem(item)
-
-            if (remainingItems.isEmpty()) return
-            remainingItems.forEach { player.inventory.addItem(it.value) }
+            totalTransfer = InventoryHelper.transferItems(blockInventory, itemType, item.amount)
+            if (totalTransfer == item.amount) {
+                player.setItemInHand(null)
+            } else {
+                item.amount = (item.amount - totalTransfer).coerceAtLeast(0)
+            }
         }
+
+        if (totalTransfer == 0) return
+
+        MessageHelper.sendMessage(
+            player,
+            MessageService.TRANSFER_SUCCESS.replaceText(
+                    TextReplacementConfig.builder()
+                        .matchLiteral("%amount")
+                        .replacement("$totalTransfer")
+                        .build()
+                )
+                .replaceText(
+                    TextReplacementConfig.builder()
+                        .matchLiteral("%item")
+                        .replacement(MessageHelper.formatString(itemType.name))
+                        .build()
+                )
+                .replaceText(
+                    TextReplacementConfig.builder()
+                        .matchLiteral("%container")
+                        .replacement(MessageHelper.formatString(block.type.name))
+                        .build()
+                ),
+        )
     }
 }
