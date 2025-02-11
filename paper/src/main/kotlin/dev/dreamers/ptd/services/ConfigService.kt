d64f309771783658f2b2c8e3d399197efbd0589b
Rafi <rafiei.default@gmail.com>
2025-02-11 18:35:13 +0330

    Fix a logical error with StorageService Add new messages tobe sent to players

--- paper/src/main/kotlin/dev/dreamers/ptd/services/ConfigService.kt
+++ paper/src/main/kotlin/dev/dreamers/ptd/services/ConfigService.kt
@@ -1,17 +1,16 @@
 package dev.dreamers.ptd.services
 
-import dev.dejvokep.boostedyaml.YamlDocument
 import dev.dreamers.ptd.helpers.StorageHelper
 
 class ConfigService private constructor() : StorageHelper("config.yml") {
 
     companion object {
-        var CONFIG_VERSION: Int = 1
-
         private val instance = ConfigService()
 
         fun init() = instance.init()
-        fun reload() = instance.reload()
+        fun reload(): Boolean = instance.reload()
+
+        var CONFIG_VERSION: Int = 1
     }
 
     override fun loadValues() {
--- paper/src/main/kotlin/dev/dreamers/ptd/services/MessageService.kt
+++ paper/src/main/kotlin/dev/dreamers/ptd/services/MessageService.kt
@@ -1,20 +1,37 @@
 package dev.dreamers.ptd.services
 
+import dev.dreamers.ptd.helpers.MessageHelper
 import dev.dreamers.ptd.helpers.StorageHelper
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextReplacementConfig
 
-class MessageService private constructor() : StorageHelper("config.yml") {
+class MessageService private constructor() : StorageHelper("messages.yml") {
 
     companion object {
-        var CONFIG_VERSION: Int = 1
-
         private val instance = MessageService()
 
         fun init() = instance.init()
-        fun reload() = instance.reload()
+        fun reload(): Boolean = instance.reload()
+        private fun setPlaceHolders(input: String): Component = instance.setPlaceHolders(input)
+
+        var PREFIX: Component = MessageHelper.colorize("<dark_gray>[<aqua>PTD<dark_gray>] <reset>")
+        var TRANSFER_SUCCESS: Component = setPlaceHolders("%prefix <green>✔ <gray>Transferred total of <white>x%amount %item <gray>into %container")
+        var UPDATE_FOUND: Component = setPlaceHolders("%prefix <red>⚠ <click:OPEN_URL:https://github.com/Dreamers-Dev/PunchToDeposit/releases><gray>A new update is available. Please <green>Update <gray>to the latest version.</click>")
+        var CONFIG_VERSION: Int = 1
+    }
+
+    private fun  setPlaceHolders(input: String): Component {
+        return MessageHelper.colorize(input)
+            .replaceText(
+                TextReplacementConfig.builder().matchLiteral("%prefix").replacement(PREFIX).build()
+            )
     }
 
     override fun loadValues() {
         val config = getConfig()
-        CONFIG_VERSION = config.getInt("config-version", 1)
+        PREFIX = setPlaceHolders(config.getString("Prefix"))
+        TRANSFER_SUCCESS = setPlaceHolders(config.getString("Transfer-Success"))
+        UPDATE_FOUND = setPlaceHolders(config.getString("Update-Found"))
+        CONFIG_VERSION = config.getInt("config-version")
     }
 }
