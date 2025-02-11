659bd3e85af142ad937551fa2064340209a1e86e
Rafi <rafiei.default@gmail.com>
2025-02-11 18:35:53 +0330

    Account for other blocks that are storages

--- paper/src/main/kotlin/dev/dreamers/ptd/helpers/InventoryHelper.kt
+++ paper/src/main/kotlin/dev/dreamers/ptd/helpers/InventoryHelper.kt
@@ -1,21 +1,55 @@
 package dev.dreamers.ptd.helpers
 
 import de.marcely.bedwars.api.BedwarsAPI
+import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.block.Chest
+import org.bukkit.block.Container
 import org.bukkit.block.EnderChest
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 class InventoryHelper {
     companion object {
+        fun transferItems(to: Inventory, material: Material, amount: Int): Int {
+            if (amount <= 0) return 0
+
+            val maxStackSize = material.maxStackSize
+            var remaining = amount
+            var transferred = 0
+
+            for ((index, item) in to.storageContents.withIndex()) {
+                if (remaining == 0) break
+
+                when {
+                    item?.type == material && item.amount < maxStackSize -> {
+                        val toAdd = minOf(maxStackSize - item.amount, remaining)
+                        item.amount += toAdd
+                        remaining -= toAdd
+                        transferred += toAdd
+                    }
+
+                    item == null -> {
+                        val toAdd = minOf(maxStackSize, remaining)
+                        to.setItem(index, ItemStack(material, toAdd))
+                        remaining -= toAdd
+                        transferred += toAdd
+                    }
+                }
+            }
+
+            return transferred
+        }
+
+
         fun getInventory(player: Player, clickedBlock: Block): Inventory? {
             val blockState = clickedBlock.state
 
             if (!HookHelper.usesMBedwars) {
                 return when (blockState) {
-                    is Chest -> blockState.inventory
                     is EnderChest -> player.enderChest
+                    is Container -> blockState.inventory
                     else -> null
                 }
             }
@@ -23,8 +57,8 @@ class InventoryHelper {
             val arena =
                 BedwarsAPI.getGameAPI().getArenaByPlayer(player)
                     ?: return when (blockState) {
-                        is Chest -> blockState.inventory
                         is EnderChest -> player.enderChest
+                        is Container -> blockState.inventory
                         else -> null
                     }
 
