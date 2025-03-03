70e567cec23820dd55307d99c0127b918945789d
Rafi <rafiei.default@gmail.com>
2025-03-03 20:37:26 +0330

    - Change version number to 1.2.0 from 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "dev.dreamers"
-    version = "1.1.0"
+    version = "1.2.0"
     description = "An example template for creating Minecraft plugins"
 
     repositories {
